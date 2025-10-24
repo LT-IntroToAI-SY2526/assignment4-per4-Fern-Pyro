d1e5c0b12f2a0e57a741690c5d056d1184fbff75
Fern-Pyro <lorellana3@cps.edu>
2025-10-24 11:06:39 -0500

    update_Dog class

--- notes.py
+++ notes.py
@@ -12,7 +12,7 @@ class Dog:
         name - the name of the dog
         age - the age of the dog
     """
-    def __init__(self, breed, fur_color, name, age): #methods every class have, even if you don't define it
+    def __init__(self, breed = "Dog", fur_color = "black", name = "no name", age = 1): #methods every class have, even if you don't define it
         """Intitialize a new Dog with breed, fur_color, name, and age"""
         self.breed = breed 
         self.fur_color = fur_color
@@ -25,12 +25,30 @@ class Dog:
     
     def bark(self):
         return f"{self.name} says: Woof, Woof"
+    
+    def birthday(self):
+        """Celebrate the dog's birthday"""
+        self.age += 1
+        print(f"{self.name} is celebrating their {self.age} birthday!")
+
+    def paint_dog(self, new_color):
+        """Changes the fur_color of the dog"""
+        old_color = self.fur_color
+        self.fur_color = new_color
+        print(f"{self.name} changed their fur color from {old_color} to {self.fur_color}")
 
 if __name__ == "__main__":
     berg_dog = Dog("labrador", "black", "logan", 9)
     aidan_dog = Dog("lab pitt mix", "grey", "cubby", 9)
-
+    default_dog = Dog()
+    mattew_dog = Dog(breed = "labrador", name = "bella", age = 1)
     print(berg_dog)
     print(aidan_dog)
+    print(default_dog)
+    print(mattew_dog)
     
     print(berg_dog.bark())
+    mattew_dog.birthday()
+    berg_dog.paint_dog("neon green")
+
+    print(berg_dog)
